71d82a0c1bdfd825f5a99256f051b9a032f8d258
Eric Cai <ericcai32@gmail.com>
2025-12-29 21:45:59 -0500

    Add import functionaliy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,21 +4,29 @@ import { Canvas } from "../components/Canvas"
 
 function App() {
   const importInputRef = useRef<HTMLInputElement>(null)
+  const [names, setNames] = useState<string[]>([])
   const [images, setImages] = useState<string[]>([])
+  const [annotations, setAnnotations] = useState<string[]>([])
+  const [currIdx, setCurrIdx] = useState<number>(0)
 
   return (
     <main className="flex h-screen w-screen gap-1 p-2">
       <Explorer
         className="h-full w-1/5 border"
         importInputRef={importInputRef}
+        names={names}
+        setNames={setNames}
         images={images}
         setImages={setImages}
+        annotations={annotations}
+        setAnnotations={setAnnotations}
+        idx={currIdx}
+        setIdx={setCurrIdx}
       />
       <Canvas
         className="h-full w-4/5 border"
         importInputRef={importInputRef}
         images={images}
-        setImages={setImages}
       />
     </main>
   )
--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -1,59 +1,88 @@
 interface ExplorerProps {
   className?: string
   importInputRef: React.RefObject<HTMLInputElement | null>
+  names: string[]
+  setNames: (names: string[]) => void
   images: string[]
   setImages: (images: string[]) => void
+  annotations: string[]
+  setAnnotations: (annotations: string[]) => void
+  currIdx: number
+  setCurrIdx: (currIdx: number) => void
 }
 
 function Explorer({
   className,
   importInputRef,
+  names,
+  setNames,
   images,
   setImages,
+  annotations,
+  setAnnotations,
+  currIdx,
+  setCurrIdx,
 }: ExplorerProps) {
-  const files: string[] = [
-    "index.html",
-    "App.tsx",
-    "Explorer.tsx",
-    "styles.css",
-    "package.json",
-    "index.html",
-    "App.tsx",
-    "Explorer.tsx",
-    "styles.css",
-    "package.json",
-    "index.html",
-    "App.tsx",
-    "Explorer.tsx",
-    "styles.css",
-    "package.json",
-    "index.html",
-    "App.tsx",
-    "Explorer.tsx",
-    "styles.css",
-    "package.json",
-    "index.html",
-    "App.tsx",
-    "Explorer.tsx",
-    "styles.css",
-    "package.json",
-    "index.html",
-    "App.tsx",
-    "Explorer.tsx",
-    "styles.css",
-    "package.json",
-  ]
-
   const handleImport = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files
-    if (files) {
-      const fileArray = Array.from(files)
-      const imageFiles = fileArray.filter((file) =>
-        file.type.startsWith("image/"),
+    if (e.target.files) {
+      const files: File[] = Array.from(e.target.files)
+      const oldNamesToContent = new Map<string, Map<string, string>>(
+        names.map((name, index) => [
+          name,
+          new Map([
+            ["image", images[index]],
+            ["annotation", annotations[index]], // TODO: add annotations
+          ]),
+        ]),
       )
-      const imageUrls = imageFiles.map((file) => URL.createObjectURL(file))
-      setImages(imageUrls)
-      console.log(imageUrls)
+      const newNamesToContent = new Map<string, Map<string, string>>()
+
+      for (const file of files) {
+        if (!newNamesToContent.has(file.name)) {
+          newNamesToContent.set(file.name, new Map<string, string>())
+        }
+        if (file.type.startsWith("image/")) {
+          newNamesToContent
+            .get(file.name)
+            ?.set("image", URL.createObjectURL(file))
+        } else if (file.type.startsWith("text/plain")) {
+          // TODO: handle annotations
+        }
+      }
+
+      const namesToContent = new Map<string, Map<string, string>>([
+        ...oldNamesToContent,
+      ])
+
+      newNamesToContent.forEach((content, name) => {
+        if (!namesToContent.has(name)) {
+          namesToContent.set(name, content)
+        } else {
+          if (!namesToContent.get(name)?.get("image")) {
+            namesToContent.get(name)?.set("image", content.get("image") ?? "")
+          }
+          if (!namesToContent.get(name)?.get("annotation")) {
+            namesToContent
+              .get(name)
+              ?.set("annotation", content.get("annotation") ?? "")
+          }
+        }
+      })
+
+      const collator = new Intl.Collator(undefined, {
+        numeric: true,
+        sensitivity: "base",
+      })
+      const sortedNamesToContent = Array.from(namesToContent.entries()).sort(
+        (a, b) => collator.compare(a[0], b[0]),
+      )
+      setNames(sortedNamesToContent.map((entry) => entry[0]))
+      setImages(
+        sortedNamesToContent.map((entry) => entry[1].get("image") ?? ""),
+      ) // TODO: add default image
+      setAnnotations(
+        sortedNamesToContent.map((entry) => entry[1].get("annotation") ?? ""),
+      )
     }
   }
 
@@ -86,14 +115,13 @@ function Explorer({
         </div>
       </header>
       <div className="overflow-auto">
-        {files.map((file, index) => (
+        {names.map((name, index) => (
           <p key={index} className="cursor-pointer p-2 hover:bg-gray-100">
-            {file}
+            {name}
           </p>
         ))}
       </div>
     </div>
   )
 }
-
 export { Explorer }
